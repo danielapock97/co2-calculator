493c8962a2fe90c629026ec0b8370598b745b2f3
Daniela Pock <daniela.pock@edu.campus02.at>
2023-03-31 10:41:06 +0200

    added user entity and user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../entities/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'http://localhost:3000/users'
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.apiUrl)
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.httpClient.get<User>(this.apiUrl + "/" + id)
+  }
+
+
+}
