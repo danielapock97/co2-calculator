ab54c6df445118976226290721a1457ddc0b52a6
Daniela Pock <daniela.pock@edu.campus02.at>
2023-04-06 21:29:11 +0200

    some css and info and about component have text

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../entities/user";
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit{
+  user!: User;
 
+  ngOnInit() {
+    this.user = JSON.parse(window.localStorage.getItem('user')!) as User
+  }
 }
